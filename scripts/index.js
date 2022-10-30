8a4282fcc0be58c9c4fe4f73c777082f0548147d
Olga Davlyud <davlyudoa@yandex.ru>
2022-10-31 01:07:00 +0300

    feat: рендер карточек из js

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,55 @@
 // Делаем выборку DOM элементов
+
+// popups
 const popupElement = document.querySelector('.popup');
-const popupButtonCloseElement = popupElement.querySelector('.popup__button-close');
+
+
+//popups open button
 const popupButtonOpenElement = document.querySelector('.profile__edit-button');
 
 
+//popup close button
+const popupButtonCloseElement = popupElement.querySelector('.popup__button-close');
+
+// popups container
 const formElement = document.querySelector('.popup__container');
+
+// popups input
 const nameInput = formElement.querySelector('.popup__input-profile-name');
 const jobInput = formElement.querySelector('.popup__input-profile-about-yourself');
 
-
+// profile container and elements
 const profileElement = document.querySelector('.profile__info');
 let nameProfile = profileElement.querySelector('.profile__name');
 let jobProfile = profileElement.querySelector('.profile__about-yourself');
 
-const popupAddElement = document.querySelector('.popup-add');
-const popupAddButtonOpenElement = document.querySelector('.profile__add-button');
-const popupAddButtonCloseElement = popupAddElement.querySelector('.popup-add__button-close');
-
-const formAddElement = document.querySelector('.popup__container');
-const nameTitleAdd = formAddElement.querySelector('.popup__input-title-name');
-const urlImage = formAddElement.querySelector('.popup-add__input-url-image');
+//  Массив карточек
+const initialCards = [
+    {
+      name: 'Дубай',
+      link: 'https://images.unsplash.com/photo-1642369455174-9a3b9f89becd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2487&q=80'
+    },
+    {
+      name: 'Москва',
+      link: 'https://images.unsplash.com/photo-1632937412284-475db917c92a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80 '
+    },
+    {
+      name: 'Стамбул',
+      link: 'https://images.unsplash.com/photo-1644989856434-641dd6036bbc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    },
+    {
+      name: 'Париж',
+      link: 'https://images.unsplash.com/photo-1591685909937-9a53ffd85f46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80'
+    },
+    {
+      name: 'Венеция',
+      link: 'https://images.unsplash.com/photo-1592803103769-f83dba4b7cbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=717&q=80'
+    },
+    {
+      name: 'Прага',
+      link: 'https://images.unsplash.com/photo-1656943460077-0fe6f61e19f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+    }
+  ];
 
 //Создаем функции
 const openPopup = function() {
@@ -53,17 +83,30 @@ function submitForm(evt) {
     closePopup();
 }
 
-const openPopupAdd = function() {
-    popupAddElement.classList.add('popup-add_opened');
+// Карточки при открытии страницы
+const containerForCards = document.querySelector('.elements');
+
+const CreateACard = (text) => {
+  return `
+  <li class="element">
+      <img class="element__image" src="./images/karachaevsk.jpg" alt="Карачаево-Черкесия">
+      <button class="element__trash-button" type="button"></button>
+      <div class="element__card">
+          <h2 class="element__title">${text}</h2>
+          <button class="element__like-button" type="button"></button>
+      </div>
+  </li>
+  `
 }
 
-const closePopupAdd = function() {
-    popupAddElement.classList.remove('popup-add_opened');
-}
+initialCards.forEach((item) => {
+  const elString = CreateACard(item);
+  containerForCards.insertAdjacentHTML('afterbegin', elString);
+});
+
+
 
 //Cлушатели событий
 popupButtonOpenElement.addEventListener('click', openPopup);
 popupButtonCloseElement.addEventListener('click', closePopup);
 formElement.addEventListener('submit', submitForm);
-popupAddButtonOpenElement.addEventListener('click', openPopupAdd);
-popupAddButtonCloseElement.addEventListener('click', closePopupAdd);
